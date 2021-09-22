a2719c3dbe37ea1a1bfdfc743cf27524c0bd8c85
mm96249624 <89981035+mm96249624@users.noreply.github.com>
2021-09-23 01:40:25 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+print("hello")
+print("hello world")
+print("       #          ")
+print("      ###         ")
+print("     #####        ")
+## Text menu in python
+def print_menu(): ## Your menu design here
+print ("1. Menu Option 1")
+print ("2. Menu Option 2")
+print ("3. Menu Option 3")
+print ("4. Menu Option 4")
+print("5. Exit")
+loop=True
+while loop: ## While loop which will keep going until loop = False
+print_menu() ## Displays menu
+choice = input("Enter your choice [1-5]: ")
+if choice==1:
+print ("Menu 1 has been selected")
+elif choice==2:
+print ("Menu 2 has been selected”)
+elif choice==3:
+print ("Menu 3 has been selected")
+elif choice==4:
+print ("Menu 4 has been selected")
+elif choice
+print ("Menu 5 has been selected")
+loop=False # This will make the while loop to end as not value of loop is set to False
+else:
+# Any integer inputs other than values 1-5 we print an error message
+raw_input("Wrong option selection. Enter any key to try again..")
+
+while(not DONE/Done):
+
+print("Total:", total);
+Item = input("Select a number or 'done': ");
+
+ if Item is “Menu option 1": total += Menu option 1;
+
+
+
+
+
